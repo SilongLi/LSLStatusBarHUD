f1a37b3b941cc575f11f2e825192c2f931eff7bb
SilongLi <lisilong@lisilongdeMacBook-Pro.local>
2015-09-22 14:14:29 +0800

    初始化框架
    这个框架主要是实现在状态栏弹出提示信息，主要解决弹出框遮住系统内容的情况。

---
+++ LSLStatusBarHUDDemo/LSLStatusBarHUDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSLStatusBarHUDDemo
+//
+//  Created by lisilong on 15/9/21.
+//  Copyright (c) 2015年 longshao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
